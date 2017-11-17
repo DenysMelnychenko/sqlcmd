6b6f6f7857c8da87e514a20a714d83960a415d97
DenysMelnychenko <aspirant89@gmail.com>
2017-11-17 19:54:07 +0200

    Added changes to test/java/juja/sqlcmd/ApplicationTest.java

---
+++ sqlcmd/src/test/java/juja/sqlcmd/ApplicationTest.java
@@ -0,0 +1,11 @@
+package juja;
+
+import juja.sqlcmd.Application;
+import org.junit.Test;
+
+public class ApplicationTest {
+    @Test
+    public void testLoadApplication() {
+        new Application();
+    }
+}
