a64bc0255cbd7947aa31dabc4ea8888aeb9054ba
DenysMelnychenko <aspirant89@gmail.com>
2017-11-28 09:20:33 +0200

    implemented showExistingTables method

--- sqlcmd/src/main/java/juja/sqlcmd/Application.java
+++ sqlcmd/src/main/java/juja/sqlcmd/Application.java
@@ -1,8 +1,6 @@
 package juja.sqlcmd;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Application {
 
@@ -10,27 +8,48 @@ public class Application {
     private static final String DB_CONNECTION_PATH = "jdbc:postgresql://127.0.0.1:5432/sqlcmd";
     private static final String DB_USER = "sqlcmd";
     private static final String DB_PASSWORD = "sqlcmd";
+    private Connection connection;
 
-    public static void main(String[] arg) {
+
+    public static void main(String[] arg) throws SQLException {
         new Application().simpleSQL();
     }
 
-    public void simpleSQL() {
+    public void simpleSQL() throws SQLException {
         checkJDBCDriver();
-        getConnection();
+        createDbConnection();
+        showExistingTables();
+
+        connection.close();
     }
 
-    private void getConnection() {
-        Connection connection;
+    private void showExistingTables() {
+        try {
+            DatabaseMetaData md = connection.getMetaData();
+            ResultSet rs = md.getTables("sqlcmd", "public", "%", null);
+            System.out.println("Existing Tables:");
+            if (rs.isBeforeFirst()) {
+                while (rs.next()) {
+                    System.out.println("-" + rs.getString(3));
+                }
+            } else {
+                System.out.println("db is empty");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void createDbConnection() {
         try {
             connection = DriverManager.getConnection(DB_CONNECTION_PATH, DB_USER, DB_PASSWORD);
         } catch (SQLException e) {
             System.out.println("Connection Failed! Check output console");
             e.printStackTrace();
-            return;
         }
         if (connection != null) {
-            System.out.println("You made it, take control your database now!");
+            System.out.println("Connection created" + "\n");
         } else {
             System.out.println("Failed to make connection!");
         }
@@ -39,7 +58,6 @@ public class Application {
     private void checkJDBCDriver() {
         System.out.println("-------- PostgreSQL "
                 + "JDBC Connection  ------------");
-
         try {
             Class.forName(POSTGRESQL_DRIVER);
         } catch (ClassNotFoundException e) {
@@ -48,9 +66,7 @@ public class Application {
                     + "Include in your library path!");
             e.printStackTrace();
         }
-
         System.out.println("PostgreSQL JDBC Driver Registered!");
-
     }
 
 }
